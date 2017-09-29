8f5712709513233dc9860370043d5a28bbe8e999
Anna Zubova <Anna@Annas-MacBook-Air.local>
2017-09-29 20:46:41 +0200

    Prototype

---
+++ script.js
@@ -0,0 +1,106 @@
+var jsonData = [{"FORMAT":"A3 landscape","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021705&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £57.99"},
+{"FORMAT":"A3 landscape","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021789&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £62.99"},
+{"FORMAT":"A3 landscape","COVER":"leather cover","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50031408&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £82.99"},
+{"FORMAT":"A4 portrait","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021696&options=%7B%7D","PRICE":"From £23.99"},
+{"FORMAT":"A4 portrait","COVER":"softcover (perfect binding)","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021708&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £18.99"},
+{"FORMAT":"A4 portrait","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021716&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £27.99"},
+{"FORMAT":"A4 portrait","COVER":"plastic cover with spiral binding","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021983&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £9.99"},
+{"FORMAT":"A4 portrait","COVER":"softcover (stapled binding)","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021706&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £9.99"},
+{"FORMAT":"A4 portrait","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021716&options=%7B%7D","PRICE":"From £27.99"},
+{"FORMAT":"A4 portrait","COVER":"leather cover","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50031407&options=%7B%7D","PRICE":"From £32.99"},
+{"FORMAT":"A4 landscape","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021697&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £23.99"},
+{"FORMAT":"A4 landscape","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021715&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £27.99"},
+{"FORMAT":"A4 landscape","COVER":"softcover (perfect binding)","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021709&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £18.99"},
+{"FORMAT":"A4 landscape","COVER":"leather cover","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50031410&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £32.99"},
+{"FORMAT":"A5 portrait","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021702&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £16.99"},
+{"FORMAT":"A5 portrait","COVER":"softcover (perfect binding)","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021711&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £15.99"},
+{"FORMAT":"A5 portrait","COVER":"plastic cover with spiral binding","PRINTING QUALITY":"standard printing","IMAGE":"red","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021984&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £8.99"},
+{"FORMAT":"A5 landscape","COVER":"softcover (perfect binding)","PRINTING QUALITY":"photographic quality","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021962&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £9.99"},
+{"FORMAT":"A5 landscape","COVER":"plastic cover with spiral binding","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021986&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £8.99"},
+{"FORMAT":"A5 landscape","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021703&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £16.99"},
+{"FORMAT":"A5 landscape","COVER":"softcover (stapled binding)","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021707&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £8.99"},
+{"FORMAT":"A5 landscape","COVER":"softcover (perfect binding)","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021712&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £15.99"},
+{"FORMAT":"A6 landscape","COVER":"softcover (perfect binding)","PRINTING QUALITY":"photographic quality","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021804&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £8.99"},
+{"FORMAT":"A6 landscape","COVER":"plastic cover with spiral binding","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021989&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £7.99"},
+{"FORMAT":"A6 landscape","COVER":"softcover (stapled binding)","PRINTING QUALITY":"standard printing","IMAGE":"green","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50032915&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £7.99"},
+{"FORMAT":"30x30 cm (12x12”)","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"blue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021701&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £42.99"},
+{"FORMAT":"30x30 cm (12x12”)","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"bue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021788&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £55.99"},
+{"FORMAT":"30x30 cm (12x12”)","COVER":"canvas","PRINTING QUALITY":"standard printing","IMAGE":"blue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50031406&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £46.99"},
+{"FORMAT":"30x30 cm (12x12”)","COVER":"leather","PRINTING QUALITY":"standard printing","IMAGE":"bue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50031409&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £66.99"},
+{"FORMAT":"20x20 cm (8x8”)","COVER":"hardcover","PRINTING QUALITY":"standard printing","IMAGE":"blue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021704&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £19.99"},
+{"FORMAT":"20x20 cm (8x8”)","COVER":"hardcover","PRINTING QUALITY":"photographic quality","IMAGE":"bue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021779&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £25.99"},
+{"FORMAT":"20x20 cm (8x8”)","COVER":"softcover (perfect binding)","PRINTING QUALITY":"standard printing","IMAGE":"blue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021710&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £15.99"},
+{"FORMAT":"20x20 cm (8x8”)","COVER":"softcover (perfect binding)","PRINTING QUALITY":"photographic quality","IMAGE":"bue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021790&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £18.99"},
+{"FORMAT":"20x20 cm (8x8”)","COVER":"plastic cover with spiral binding","PRINTING QUALITY":"standard printing","IMAGE":"blue","TARGET LINK (EDITOR)":"https://designer.myphotobook.co.uk/products/editor.html?locale=en_GB&productId=50021987&options=%7B%7D#/wizardWorkflowSelection/0","PRICE":"From £8.99"}];
+
+
+function update() {
+	var format = $('#format').val();
+	var cover = $('#cover').val();
+	var printing = $('#printing-quality').val();
+	console.log(format);
+	console.log(cover);
+	console.log(printing);
+
+	if (_.isString(format) && _.isString(cover) && _.isString(printing) && format != "" && cover != "" && printing != "") {
+		console.log('all selected');
+		var option = findOption(format, cover, printing);
+		console.log(option);
+		if (option) {
+			display(option);
+		} else {
+			disableBtn();
+		}
+	} else {
+		console.log('not all selected');
+	}
+}
+
+function display(option) {
+	$('#image-box img').attr('src', option.IMAGE + '.jpg');
+	$('#image-box a').attr('href', option['TARGET LINK (EDITOR)']);
+	$('#image-box a').attr('class', 'btn btn-success');
+	$('#price').html(option.PRICE);
+}
+
+function disableBtn() {
+	$('#image-box a').attr('class', 'invisible');
+	$('#price').html('From £--');
+}
+
+function findOption(format, cover, printing) {
+	var options = _.filter(jsonData, function (item) {
+		console.log(item);
+		return (item.FORMAT == format && item.COVER == cover && item["PRINTING QUALITY"] == printing);
+	});
+
+	console.log(options);
+
+	if (options.length != 0) {
+		return options[0];
+	} else {
+		return false;
+	}
+}
+
+function printOptions() {
+	var formats = [];
+	var covers = [];
+	var printings = [];
+	_.map(jsonData, function (val) {
+		formats = _.union(formats, [val.FORMAT]);
+		covers = _.union(covers, [val.COVER]);
+		printings = _.union(printings, [val["PRINTING QUALITY"]]);
+	});
+
+	console.log('Formats:', formats);
+	console.log('Covers:', covers);
+	console.log('Printing quality:', printings);
+}
+
+printOptions();
+
+$('#format').change(update);
+$('#cover').change(update);
+$('#printing-quality').change(update);
+
